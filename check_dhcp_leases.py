59543452cf3a61cf880260c86b4444ab18e460e0
Ian Chard <ian@chard.org>
2020-09-15 15:12:21 +0100

    Python 3 compatibility

--- check_dhcp_leases.py
+++ check_dhcp_leases.py
@@ -1,5 +1,3 @@
-#!/usr/pkg/bin/python2.7
-
 from netaddr import iter_iprange
 import itertools
 import json
@@ -8,8 +6,8 @@ import pypureomapi
 import re
 import sys
 
-KEYNAME = 'omapi_key'
-BASE64_ENCODED_KEY = 'g8pw3Kc7NfbXMEaMg=='
+KEYNAME = b'omapi_key'
+BASE64_ENCODED_KEY = b'PUT_YOUR_BASE64_KEY_IN_HERE'
 dhcp_server_ip = '127.0.0.1'
 port = 7911 # Port of the omapi service
 OMAPI_OP_UPDATE = 3
@@ -72,17 +70,17 @@ def checkRange(ipsList, ip_type):
 
     o = pypureomapi.Omapi(dhcp_server_ip, port, KEYNAME, BASE64_ENCODED_KEY)
     for ip in ips:
-        msg = pypureomapi.OmapiMessage.open('lease')
-        msg.obj.append(('ip-address', pypureomapi.pack_ip(str(ip))))
+        msg = pypureomapi.OmapiMessage.open(b'lease')
+        msg.obj.append((b'ip-address', pypureomapi.pack_ip(str(ip))))
         response = o.query_server(msg)
         if response.opcode != OMAPI_OP_UPDATE:
-            print 'ZBX_NOTSUPPORTED'
+            print('ZBX_NOTSUPPORTED')
         # Need to dig for the key error thingy
         try:
             state = ord(response.obj[0][1][-1:])
             results[leases_states[state]] += 1
         except KeyError:
-            print 'ZBX_NOTSUPPORTED'
+            print('ZBX_NOTSUPPORTED')
 
     print(results[ip_type])
 
